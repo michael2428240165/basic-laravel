3fa0570b0dbcc98092d27f7d918bda263ac3d30c
michael2428240165 <michael_2428240165@mhs.mdp.ac.id>
2026-05-02 20:42:21 +0700

    first commit

---
+++ resources/views/fakultas/list-fakultas.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>List Fakultas</h1>
+    <a href="/fakultas">list fakultas</a>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite([])
-</head>
-<body>
-    
-    <h1>Belajar Welcome</h1>
-
-</body>
-</html>
+<x-layout>
+    <h1>Welcome</h1>
+    <a href="#">List Fakultas</a>
+</x-layout>

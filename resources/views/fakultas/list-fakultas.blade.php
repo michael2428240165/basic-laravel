7c886d5d24a719e5071abf22d4f90bd01273bfa8
michael2428240165 <michael_2428240165@mhs.mdp.ac.id>
2026-05-09 20:36:04 +0700

    update project

--- resources/views/fakultas/list-fakultas.blade.php
+++ resources/views/fakultas/list-fakultas.blade.php
@@ -1,4 +1,32 @@
 <x-layout>
     <h1>List Fakultas</h1>
-    <a href="/fakultas">list fakultas</a>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Fakultas</th>
+                <th>Dekan</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($fakultas as $item)
+            <tr>
+                <td>{{$loop->interation}}</td>
+                <td>{{$item->nama_fakultas}}</td>
+                <td>{{$item->nama_dekan}}</td>
+                <td>
+                    <a href="/fakultas/{{ $item->e}}" class="btn btn-warning">Edit</a>
+                    <form action="/fakultas/{{ $item->id}}" method ="post">
+                        @csrf
+                        @method("DELETE")
+                        <button type="submit" class ="btn btn-danger">
+                            HAPUS
+                        </button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/fakultas', function(){
-    return view('fakultas.list-fakultas');
-});
+// Route::get('/fakultas', function(){
+//     return view('fakultas.list-fakultas');
+// });
+
+// Route::get('/fakultas', function(){
+//     return view('fakultas.list-fakultas');
+// });
